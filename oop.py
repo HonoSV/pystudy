1a19aa8b3abd1ce245c0fa4154c97ef19bc8b64e
HonoSV <thbb268@live.com>
2018-01-05 17:46:02 +0800

    2018-01-05

---
+++ oop.py
@@ -0,0 +1,39 @@
+# -*_ coding:utf-8 -*-
+
+
+class Student(object):
+
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.__name, self.__score))
+
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >=60:
+            return 'B'
+        else:
+            return 'c'
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.get_score()
+
+    def set_score(self,score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            return ValueError('bad score')
+
+
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson',87)
+bart.print_score()
+lisa.print_score()
+
+print(lisa.get_name(),lisa.get_grade())
