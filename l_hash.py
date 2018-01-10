0e796556415e616cecbc72535af7956a9f7e01e8
HonoSV <thbb268@live.com>
2018-01-10 17:33:14 +0800

    2018-01-10

---
+++ l_hash.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+'''
+import hashlib
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+
+
+def login(user, password):
+    md5 = hashlib.md5()
+    md5.update(password.encode('utf-8'))
+    tmp = md5.hexdigest()
+    flag = (tmp == db[user])
+    return flag
+
+
+assert login('michael', '123456')
+assert login('bob', 'abc999')
+assert login('alice', 'alice2008')
+assert not login('michael', '1234567')
+assert not login('bob', '123456')
+assert not login('alice', 'Alice2008')
+print('ok')
+'''
+
+import hmac, random
+
+
+def hmac_md5(key, s):
+    return hmac.new(key.encode('utf-8'), s.encode('utf-8'), 'MD5').hexdigest()
+
+
+class User(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.key = ''.join([chr(random.randint(48, 122)) for i in range(20)])
+        self.password = hmac_md5(self.username+self.key, password)
+db = {
+    'michael': User('michael', '123456'),
+    'bob': User('bob', 'abc999'),
+    'alice': User('alice', 'alice2008')
+}
+
+
+def login(username, password):
+    user = db[username]
+
+    return user.password == hmac_md5(username+user.key, password)
+
+
+# 测试:
+assert login('michael', '123456')
+assert login('bob', 'abc999')
+assert login('alice', 'alice2008')
+assert not login('michael', '1234567')
+assert not login('bob', '123456')
+assert not login('alice', 'Alice2008')
+print('ok')
